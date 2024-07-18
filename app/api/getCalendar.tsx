0a2ad2efe7e2b736fabc9e58f15de17fa4ed1635
Isaque Böck <isaquebockdos@gmail.com>
2024-07-18 10:52:26 -0300

    fix: remove console.log

--- app/api/getCalendar.tsx
+++ app/api/getCalendar.tsx
@@ -2,8 +2,7 @@ import { Calendar } from "../model/calendar.model";
 
 export async function getCalendar(): Promise<Calendar[]> {
     const res = await fetch(`https://psidofuturo-lxlo.vercel.app/calendar`)
-    
-    console.log(url)
+
     
     if(!res.ok) {
         throw new Error('Não foi possivel carregar o calendário!');
